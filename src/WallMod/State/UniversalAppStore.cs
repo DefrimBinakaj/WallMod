073282af1b38561725469673cd6450321712658d
Defrim Binakaj <defrim.binakaj@gmail.com>
2025-07-03 15:22:08 -0600

    smooth scrolling added

--- src/WallMod/State/UniversalAppStore.cs
+++ src/WallMod/State/UniversalAppStore.cs
@@ -306,7 +306,7 @@ public class UniversalAppStore : ViewModelBase
     }
 
     // version ===================================================
-    private string appNameVersion = "v0.0.13";
+    private string appNameVersion = "v0.0.14";
     public string AppNameVersion
     {
         get => appNameVersion;
